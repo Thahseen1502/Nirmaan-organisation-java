32db7a882178a6eedbf811ad119406c1904a8b0f
Thahseen <126459225+Thahseen1502@users.noreply.github.com>
2024-11-26 13:12:31 +0530

    Add files via upload
    day5

---
+++ loops.java
@@ -0,0 +1,44 @@
+package day3;
+import java.util.Scanner;
+public class loops {
+	
+	public static void main(String args[]) {
+		
+		//Nested-if
+		
+//		Scanner scan=new Scanner(System.in);
+//		System.out.println("Enter your marks:");
+//		int marks = scan .nextInt();
+//		if(marks>=35 ) {
+//		if(marks>=90) {
+//			System.out.println("Grade A");
+//			}
+//		else if(marks>=70) {
+//			System.out.println("Grade B");
+//		}
+//		else if(marks>=50) {
+//			System.out.println("Grade C");
+//		}
+//		else {
+//		System.out.println("Grade D");
+//		}
+//		}
+//		else {
+//			System.out.println("Fail");
+//		}
+//		
+		
+		//Tables for loop
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter number:");
+		int num = scan.nextInt();
+		
+		
+		for(int i=1;i<=10;i++) {
+			System.out.println(num+"x"+i+"="+i*num);
+		}
+		
+	}
+
+}
+

29919823d6382e3205379385e18793f7cf2c16fb
Thahseen <126459225+Thahseen1502@users.noreply.github.com>
2024-12-03 12:58:00 +0530

    day 8

---
+++ pattern.java
@@ -0,0 +1,33 @@
+package day8;
+
+public class pattern {
+
+	public static void main(String[]args) {
+		
+		
+//	//* pattern	
+//	for(int i=0;i<=3;i++) {
+//		for(int j=0;j<=3;j++) {
+//			System.out.print("*");
+//		}
+//		System.out.println();
+//	}
+//	
+		
+		
+		//Vaa+rnamayiram
+		
+		
+		String name ="Surya";
+		String str = new String("Surya");
+		
+		if(name.equals(str)) {
+			System.out.println("Surya weds Thahseen");
+		}else {
+			System.out.println("Surya weds jothika");
+		}
+		
+		
+	
+	}
+}
